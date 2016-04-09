ca0ba1245fa01d38acf630cc271ac68d6364644c
CAN GUMELI <cgumeli@parcore-6-0.local>
2016-04-09 18:57:19 +0300

    First versions of encoder and decoder are coded

---
+++ decoder.jl
@@ -0,0 +1,10 @@
+using Knet
+
+@knet function embedder(x; output=512)
+    return wdot(x; out=512)
+end
+
+@knet function decoder(x; dict_size=0)    
+    h = lstm(x; out=dict_size)
+    return soft(h)
+end
---
+++ encoder.jl
@@ -0,0 +1,42 @@
+using Knet
+
+@knet function vgg_generic_conv(x; cinput=0, coutput=0)
+    w = par(init=Xavier(), dims=(3, 3, cinput, coutput))
+    b = par(init=Constant(0), dims=(1,1,coutput,1))
+    c = conv(w, x; padding=1)
+    return relu(c)
+end
+
+@knet function layer_2conv(x0; output=64)
+    x1 = vgg_generic_conv(x0; coutput=output)
+    x2 = vgg_generic_conv(x1; coutput=output)
+    return pool(x2)
+end
+
+@knet function layer_3conv(x0; output=64)
+    x1 = vgg_generic_conv(x0; coutput=output)
+    x2 = vgg_generic_conv(x1; coutput=output)
+    x3 = vgg_generic_conv(x2; coutput=output)
+    return pool(x3)
+end
+
+@knet function layer_fcdrop(x0)
+    x1 = wbf(x0; f=:relu, out=4096)
+    return drop(x1; pdrop=0.5)
+end
+
+
+@knet function vgg16(xl0; output=512)
+    #Convolutional layers
+    xl1 = layer_2conv(xl0; output=64)    
+    xl2_ = layer_2conv(xl1; output=128)    
+    xl3 = layer_3conv(xl2; output=256)    
+    xl4 = layer_3conv(xl3; output=512)    
+    xl5 = layer_3conv(xl4; output=512)
+
+    #Fully Connected layers
+    xl6 = layer_fcdrop(xl5)
+    xl7 = layer_fcdrop(xl6)
+    
+    return wbf(xl7; f=:soft, out=output)
+end
