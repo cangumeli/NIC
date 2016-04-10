68cd943c75d372c8bbe1b380045eeec2030d4e7b
CAN GUMELI <cgumeli@parcore-6-0.local>
2016-04-11 01:49:19 +0300

    Coordinate system changed to col-major

--- img_utils.jl
+++ img_utils.jl
@@ -2,7 +2,7 @@ using Images, ImageMagick
 
 #takes an image as input and transform it into an array
 function img2vec(img)
-    r = convert(Array{Float64, 3}, raw(img))
+    r = convert(Array{Float64, 3}, raw(img'))
     I = zeros(size(r)[2], size(r)[3], 3, 1)
     I[:,:,1,1] = r[1,:,:]
     I[:,:,2,1] = r[2,:,:]
