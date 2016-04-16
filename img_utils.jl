847cd140847438a8aba6d1d8b67fa0f6d1c51fa8
CAN GUMELI <cgumeli@parcore-6-0.local>
2016-04-16 04:47:18 +0300

    Some useful prep. stuff added

--- img_utils.jl
+++ img_utils.jl
@@ -1,8 +1,15 @@
 using Images, ImageMagick
 
 #takes an image as input and transform it into an array
-function img2vec(img)
-    r = convert(Array{Float64, 3}, raw(img'))
+function img2vec(img; crop=true, crop_target=224, rescale = false, minmin_scale=256, minmax_scale=256, flip=true)
+    rescale && (img = random_rescale(img; srange=minmin_scale:minmax_scale))
+    if crop
+        x_start = rand(1:size(img,1)-crop_target+1)
+        y_start = rand(1:size(img,2)-crop_target+1)
+        img = subim(img, "x", x_start:x_start+crop_target-1, "y", y_start:y_start+crop_target-1)
+    end
+    flip && rand(1:2)==1 && flipdim(img, 1)
+    r = convert(Array{Float16, 3}, raw(img'))
     I = zeros(size(r)[2], size(r)[3], 3, 1)
     I[:,:,1,1] = r[1,:,:]
     I[:,:,2,1] = r[2,:,:]
@@ -13,6 +20,30 @@ end
 #Apply mean subtraction to the image
 function mean_subtract(img)
     return img .- mean(img,3)
+end
+
+#Random rescaling system that applies isomorphic rescaling
+function random_rescale(img; srange=256:512)
+    S = rand(srange)
+    s1, s2 = size(img)
+    scale = S/min(s1,s2)
+    s1 = convert(Int, ceil(s1*scale))
+    s2 = convert(Int, ceil(s2*scale))
+
+    return Images.imresize(img, (s1, s2))
+end
+
+
+#Random cropping operation
+function random_crop(img_vector; target=(224,224))
+    cropped = zeros(target[1], target[2], size(img_vector,3), size(img_vector,4))
+    for i = 1:size(img_vector,3)        
+        d1_start = rand(1:size(img_vector,1)-target[1]+1)
+        d2_start = rand(1:size(img_vector,2)-target[2]+1)    
+        cropped[:, :, i, :] = img_vector[d1_start:d1_start+target[1]-1,
+                                         d2_start:d2_start+target[2]-1, i, :]
+    end
+    return cropped
 end
 
 
