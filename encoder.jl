c9981854aec8b3dd3fccc105264ae3d126acf765
cangumeli <cgumeli@ku.edu.tr>
2016-04-21 00:42:34 +0300

    Weights are initialized with a pretrained model successfully.

--- encoder.jl
+++ encoder.jl
@@ -3,47 +3,69 @@
 
 using Knet
 
-@knet function vgg_generic_conv(x; cinput=0, coutput=0)
-    w = par(init=Xavier(), dims=(3, 3, cinput, coutput))
-    b = par(init=Constant(0), dims=(1,1,coutput,1))
+include("weigth_initializer.jl")
+ws, bs = get_weights()
+
+#bs[1] = reshape(bs[1], (1,1,64,1))
+#ws = reshape(ws, (length(ws),1))
+
+#bs = reshape(bs, (length(bs),1))
+
+@knet function vgg_generic_conv(x; winits=ws, binits=bs, wi=1, bi=1, out=64)
+    w = par(init=winits[wi], dims=size(winits[wi]))                                              
+    b = par(init=binits[bi], dims=size(binits[bi]))
+    
     c = conv(w, x; padding=1)
     cb = c + b
     return relu(cb)
 end
 
-@knet function layer_2conv(x0; output=64)
-    x1 = vgg_generic_conv(x0; coutput=output)
-    x2 = vgg_generic_conv(x1; coutput=output)
+@knet function layer_2conv(x0; start_index=1)
+    x1 = vgg_generic_conv(x0; wi=start_index, bi=start_index)
+    x2 = vgg_generic_conv(x1;  wi=start_index+1, bi=start_index+1)
     return pool(x2)
 end
 
-@knet function layer_3conv(x0; output=64)
-    x1 = vgg_generic_conv(x0; coutput=output)
-    x2 = vgg_generic_conv(x1; coutput=output)
-    x3 = vgg_generic_conv(x2; coutput=output)
+@knet function layer_3conv(x0; start_index=1)
+    x1 = vgg_generic_conv(x0; wi=start_index, bi=start_index)
+    x2 = vgg_generic_conv(x1; wi=start_index+1, bi=start_index+1)
+    x3 = vgg_generic_conv(x2; wi=start_index+2, bi=start_index+2)
     return pool(x3)
 end
 
-@knet function layer_fcdrop(x0)
-    x1 = wbf(x0; f=:relu, out=4096)
+@knet function layer_fcdrop(x0; winits=ws, binits=bs, start_index=1)
+    x1 = wbf(x0; f=:relu,
+             winit=winits[start_index],
+             binit=binits[start_index]
+             )
+#    w = par(init=winits[start_index], dims=size(winits[start_index]))
+    #x1 = w * x0
     return drop(x1; pdrop=0.5)
 end
 
 
-@knet function vgg16(xl0; output=512)
+@knet function vgg16(xl0)
     #Convolutional layers
-    xl1 = layer_2conv(xl0; output=64) 
-    xl2 = layer_2conv(xl1; output=128)
-    xl3 = layer_3conv(xl2; output=256)    
-    xl4 = layer_3conv(xl3; output=512)    
-    xl5 = layer_3conv(xl4; output=512)
+    xl1 = layer_2conv(xl0;  start_index=1) 
+    xl2 = layer_2conv(xl1;  start_index=3)
+    xl3 = layer_3conv(xl2;  start_index=5)    
+    xl4 = layer_3conv(xl3;  start_index=8)
+    xl5 = layer_3conv(xl4;  start_index=11)
 
     #Fully Connected layers
-    xl6 = layer_fcdrop(xl5)
-    xl7 = layer_fcdrop(xl6)
+    xl6 = layer_fcdrop(xl5; start_index=14)
+    xl7 = layer_fcdrop(xl6, start_index=15)
 
-    #Embedding layer for image representation
-    return wdot(xl7; out=output)
+    ##Embedding layer for image representation
+    return xl7
+    #return wdot(xl7; out=output)
 
     # return wbf(xl7; f=:soft, out=output)
 end
+
+@knet function top_layer(x; output=512)
+    return wdot(x; output=output)
+end
+
+f = compile(:vgg16)
+forw(f, rand(Float32,224, 224, 3, 5))
---
+++ weigth_initializer.jl
@@ -0,0 +1,47 @@
+using MAT, Knet
+
+function get_weights(dst="/mnt/kufs/scratch/cgumeli/NIC/imagenet-vgg-verydeep-16.mat", fc_start=14, weight_type=Float32)
+    file = matopen(dst)
+    global layers = read(file, "layers")
+    ws = Any[]
+    bs = Any[]
+    for i=1:length(layers)
+        l = layers[i]
+        if haskey(l, "weights")
+            #w = l["weights"][1]
+            #b = l["weights"][2]
+            if length(layers) >= fc_start && false
+                sw = size(l["weights"][1])
+                l["weights"][1] = reshape(l["weights"][1],
+                                          (sw[1]*sw[2]*sw[3],sw[4]))'
+                #l["weights"][2] = reshape(l["weights"][1],                                      
+                #println(size(w))
+                #b = reshape(b, (length(b),1))
+                #println(size(b))
+                push!(ws, convert(Array{weight_type,2},l["weights"][1]))
+            end
+
+            if length(ws) < fc_start-1
+               # println(i)
+                push!(ws, convert(Array{weight_type,4},l["weights"][1]))
+            #push!(ws, convert(Array{weight_type,4},l["weights"][2]))                            
+                push!(bs, convert(Array{weight_type,4},reshape(l["weights"][2], (1,1,length(l["weights"][2]),1))))
+            else #fully connected
+                sw = size(l["weights"][1])
+                sw_ = (sw[1]*sw[2]*sw[3], sw[4])
+                push!(ws,
+                      convert(Array{weight_type, 2},
+                              reshape(l["weights"][1], sw_)')) #make w 2-D
+                push!(bs, convert(Array{weight_type,1},
+                                  l["weights"][2][:])) #make b 1-D
+                #println(size(last(ws)))
+                #println(size(last(bs)))
+                #println()
+            end
+            
+        end
+    end 
+    close(file)
+    return ws, bs
+end
+    
